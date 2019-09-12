d0ae05c7ce358d147c1a8e4db8b3b9a8d86d876c
peachyshroom <42749988+peachyshroom@users.noreply.github.com>
2019-09-12 17:36:14 -0400

    sketch

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,10 +1,25 @@
+/*comments*/ 
 function setup () {
   createCanvas(600, 400);  
 }
 
 function draw() {
     background("#f9beff");
-    circle(300, 200, 100)
-    ellipse (200, 100, 10, 150)
+    /*fill();
+    noFill();
+    stroke();
+    noStroke();
+    strokeWeight();*/ 
+    
+  arc(300, 200, 350, 350, 2.5, 1);
+    circle(300, 200, 250);//face
+    
+    triangle(380, 190, 330, 170, 480, 100);
+    
+    triangle(290, 165, 110, 50, 230, 200)
+    
+    triangle()
+    /*ellipse (200, 100, 10, 150);*/ 
+
 }
 

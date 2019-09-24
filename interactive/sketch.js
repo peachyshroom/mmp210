ed0359e4dc01211555b287acb757e7217108de2a
peachyshroom <42749988+peachyshroom@users.noreply.github.com>
2019-09-24 15:38:17 -0400

    variables

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -11,19 +11,20 @@ function draw() {
     noStroke();
     strokeWeight();*/ 
     var hairColor = "#ff4dc6"
-    var 
+    var x = 300
+    var y = 200
     fill(hairColor);
-  arc(300, 200, 350, 350, 2.5, 1);
+  arc(300, 200, x + 50 , x + 50, 2.5, 1);//hair
     
     fill("white");
     circle(300, 200, 250);//face
     
     fill("black");
-    triangle(380, 190, 330, 170, 480, 100);
+    triangle(x + 80, y - 10, x + 30, y - 30, x + 180, y - 100);
     
-    triangle(290, 165, 110, 50, 230, 200);
+    triangle(x - 10, y - 35, x - 190, y - 150, x - 70 , 200);
     
-    triangle(300,200, 250, 250, 350,250)
+    triangle(300,200, x - 50, y + 50, x + 50,y + 50)
     /*ellipse (200, 100, 10, 150);*/ 
 
 }

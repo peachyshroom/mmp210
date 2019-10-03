f18c08abb0f4e97bdce7581875612b1e8bd52419
peachyshroom <42749988+peachyshroom@users.noreply.github.com>
2019-10-03 14:02:59 -0400

    Update sketch.js
    iuredhj

--- interactive/sketch.js
+++ interactive/sketch.js
@@ -1,6 +1,6 @@
 /*comments*/ 
 function setup () {
-  createCanvas(600, 400);  
+  createCanvas(800, 800);  
 }
 
 function draw() {
@@ -13,19 +13,24 @@ function draw() {
     var hairColor = "#ff4dc6"
     var x = 300
     var y = 200
+    var x = mouseX;
+    var y = mouseY;
+    
     fill(hairColor);
-  arc(300, 200, x + 50 , x + 50, 2.5, 1);//hair
+  arc(x, y, x + 50 , x + 50, 2.5, 1);//hair
     
     fill("white");
-    circle(300, 200, 250);//face
+    circle(x, y, 250);//face
     
     fill("black");
     triangle(x + 80, y - 10, x + 30, y - 30, x + 180, y - 100);
     
     triangle(x - 10, y - 35, x - 190, y - 150, x - 70 , 200);
     
-    triangle(300,200, x - 50, y + 50, x + 50,y + 50)
+    triangle(x, y, x - 50, y + 50, x + 50,y + 50)
     /*ellipse (200, 100, 10, 150);*/ 
 
 }
+
+
 
